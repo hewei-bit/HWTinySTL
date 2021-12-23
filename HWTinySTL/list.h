b89b19fd8dace46f4337610abd0d31c0774f836b
hewei-bit <1003826976@qq.com>
2021-12-24 00:56:42 +0800

    模板类 list

--- HWTinySTL/list.h
+++ HWTinySTL/list.h
@@ -20,5 +20,25 @@
 //   * push_front
 //   * push_back
 //   * insert
+#include <initializer_list>
 
+#include "iterator.h"
+#include "memory.h"
+#include "functional.h"
+#include "util.h"
+#include "exceptdef.h"
+namespace hwstl
+{
+    template <calss T>
+    struct list_node_base;
+    template <class T>
+    struct list_node;
+    template <class T>
+    struct node_traits
+    {
+        typedef list_node_base<T> *base_ptr;
+        typedef list_node<T> *node_ptr;
+    }
+
+}
 #endif
