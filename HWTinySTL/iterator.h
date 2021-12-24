6ba913dfafefb8a3704ea0a4e44fe661506fa134
hewei-bit <1003826976@qq.com>
2021-12-24 21:28:33 +0800

    迭代器

--- HWTinySTL/iterator.h
+++ HWTinySTL/iterator.h
@@ -1,8 +1,27 @@
 /**
  * @File Name: iterator.h
- * @Brief :
- * @Author : name (xx@foxmail.com)
+ * @Brief : 用于迭代器设计，包含了一些模板结构体与全局函数，
+ * @Author : hewei (hewei_1996@qq.com)
  * @Version : 1.0
- * @Creat Date : 2021-12-19
+ * @Creat Date : 2021-12-24
  *
  */
+
+#include <cstddef>
+
+#include "type_traits.h"
+namespace hwstl
+{
+    //五种迭代器:
+    struct input_iterator_tag
+    {
+    };
+    struct output_iterator_tag
+    {
+    };
+    struct forward_iterator_tag : public input_iterator_tag
+    {
+    };
+    struct bi
+
+}
