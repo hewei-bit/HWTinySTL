2e4661deb967042199f2117d692f51ec408a7b7f
hewei-bit <1003826976@qq.com>
2021-12-25 21:07:23 +0800

    新建通用工具

--- HWTinySTL/util.h
+++ HWTinySTL/util.h
@@ -12,8 +12,14 @@ namespace hwstl
     template <class T>
     typename std::remove_reference<T>::type &&move(T &&arg) noexcept
     {
+        return static_cast<typename std::remove_reference<T>::type &&>(arg);
     }
     // forward
+    template <class T>
+    T &&forward(typename std::remove_reference<T>::type &arg) noexcept
+    {
+        return static_cast<T &&>(arg);
+    }
 
     // swap
 
