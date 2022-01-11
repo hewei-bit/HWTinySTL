ef1996b374d2542ed15e5f9e6948a386eae6bfd9
hewei-bit <1003826976@qq.com>
2022-01-12 00:37:38 +0800

    分配器

--- HWTinySTL/allocator.h
+++ HWTinySTL/allocator.h
@@ -26,13 +26,88 @@ namespace hwstl
 
     public:
         static T *allocate();
-        static T allocate(size_t);
+        static T *allocate(size_type n);
 
         static void deallocate(T *ptr);
         static void deallocate(T *ptr, size_type n);
 
-        static void construct
+        static void construct(T *ptr);
+        static void construct(T *ptr, const T &value);
+        static void construct(T *ptr, T &&value);
+
+        template <typename... Args>
+        static void construct(T *ptr, Args &&...args);
+
+        static void destroy(T *ptr);
+        static void destroy(T *first, T *last);
     };
+
+    template <typename T>
+    inline T *allocator<T>::allocate()
+    {
+        return static_cast<T *>(::operator new(sizeof(T)));
+    }
+
+    template <typename T>
+    inline T *allocator<T>::allocate(size_type n)
+    {
+        if (n == 0)
+            return nullptr;
+        return static_cast<T *>(::operator new(n * sizeof(T)));
+    }
+
+    template <typename T>
+    inline void allocator<T>::deallocate(T *ptr)
+    {
+        if (ptr == nullptr)
+            return;
+        ::operator delete(ptr);
+    }
+
+    template <typename T>
+    inline void allocator<T>::deallocate(T *ptr, size_type /*size*/)
+    {
+        if (ptr == nullptr)
+            return;
+        ::operator delete(ptr);
+    }
+
+    template <typename T>
+    inline void allocator<T>::construct(T *ptr)
+    {
+        hwstl::construct(ptr);
+    }
+
+    template <typename T>
+    inline void allocator<T>::construct(T *ptr, const T &value)
+    {
+        hwstl::construct(ptr, value);
+    }
+
+    template <typename T>
+    inline void allocator<T>::construct(T *ptr, T &&value)
+    {
+        hwstl::construct(ptr, hwstl::move(value));
+    }
+
+    template <typename T>
+    template <typename... Args>
+    inline void allocator<T>::construct(T *ptr, Args &&...args)
+    {
+        hwstl::construct(ptr, hwstl::forward<Args>(args)...);
+    }
+
+    template <typename T>
+    inline void allocator<T>::destroy(T *ptr)
+    {
+        hwstl::destroy(ptr);
+    }
+
+    template <typename T>
+    inline void allocator<T>::destroy(T *first, T *last)
+    {
+        hwstl::destroy(first, last);
+    }
 }
 
 #endif
