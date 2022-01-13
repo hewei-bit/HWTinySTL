a1e1c725dd45295acfd8902d02c06cde79787f0f
hewei-bit <1003826976@qq.com>
2022-01-13 14:46:41 +0800

    更新

--- HWTinySTL/memory.h
+++ HWTinySTL/memory.h
@@ -6,8 +6,7 @@
  * @Author : hewei (hewei_1996@qq.com)
  * @Version : 1.0
  * @Creat Date : 2022-01-03
- *
- *
+ * @State : finished 2022-01-13
  */
 
 #include <cstddef>
@@ -18,5 +17,20 @@
 #include "allocator.h"
 #include "construct.h"
 #include "uninitialized.h"
+namespace hwstl
+{
+    //获取对象地址
+    template <typename Tp>
+    inline constexpr Tp *addressof(Tp &value) noexcept
+    {
+        return &value;
+    }
 
+    //获取 释放 临时缓冲区
+    template <typename T>
+    inline pair<T *, ptrdiff_t> get_buffer_helper(ptrdiff_t len, T *)
+    {
+        if ()
+    }
+}
 #endif
